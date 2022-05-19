a21d209d04b3bee176177c0092ef5c50600823b0
safiulalam99 <safiulalam99>
2022-05-20 02:58:39 +0300

    adding routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { CartPage } from '../pages/CartPage'
 import Error from '../pages/Error'
 import Home from '../pages/Home'
 import ResponsiveAppBar from '../components/NavBar'
+import Country from '../pages/Country'
 
 const Index = () => {
   return (
@@ -13,6 +14,7 @@ const Index = () => {
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/*" element={<Error />} />
+        <Route path="/country/:id" element={<Country />} />
       </Routes>
     </BrowserRouter>
   )
